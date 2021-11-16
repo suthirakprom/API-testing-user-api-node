e8253ce98d2d6e508e482692d758094778b91897
Suthirak Prom <jinjason@Suthiraks-Air.mshome.net>
2021-11-16 10:55:29 +0700

    contain docker image

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      name: "Bob",
+      email: "bob@gmail.com",
+    },
+    {
+      name: "Alice",
+      email: "alice@gmail.com",
+    },
+    {
+      name: "Jin",
+      email: "jin@hotmail.com",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
